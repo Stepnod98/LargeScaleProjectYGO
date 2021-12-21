3cf9982751c0cdd5a23c79786c422d8ec3154119
Stepnod98 <87202385+Stepnod98@users.noreply.github.com>
2021-12-21 18:50:52 +0100

    Add files via upload

---
+++ GUITestYGO/src/guitestygo/LoginManager.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guitestygo;
+
+/**
+ *
+ * @author Stefano
+ */
+public class LoginManager {
+    public static void login(){
+        
+        GUIManager.openLoginManager();
+    }
+    
+    public static void logout(){
+        GUIManager.openLoginManager();
+    }
+    
+    public static void signup(){
+        
+    }
+}
